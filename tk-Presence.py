baf1abdda6860b1762ecd81bccb94b98182ae958
anytarsier67 <42589471+anytarsier67@users.noreply.github.com>
2020-12-21 10:46:19 -0500

    Add files via upload
    better layout
    added quit button
    added entry for large and small images

--- tk-Presence.py
+++ tk-Presence.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from pypresence import Presence
+import sys
 
 client_id = '''client id here'''
 RPC = Presence(client_id,pipe=0)
 RPC.connect()
 
 root = tk.Tk()
-root.rowconfigure([0, 1, 2], minsize=50, weight=1)
-root.columnconfigure([0, 1, 2], minsize=50, weight=1)
+root.rowconfigure([0, 1, 2, 3], minsize=50, weight=1)
+root.columnconfigure([0, 1, 2, 3], minsize=50, weight=1)
 
 label1 = tk.Label(master=root, text="details")
 label1.grid(row=0, column=0)
@@ -16,13 +17,37 @@ label2.grid(row=0, column=1)
 
 entry = tk.Entry(master=root, fg="black", bg="blue")
 entry2 = tk.Entry(master=root, fg="black", bg="blue")
+
+small = tk.Entry(master=root, fg="black", bg="blue")
+large = tk.Entry(master=root, fg="black", bg="blue")
+small_label = tk.Label(master=root, text="small image")
+small_label.grid(row=2, column=0)
+large_label = tk.Label(master=root, text="large image")
+large_label.grid(row=2, column=1)
+
 def conv():
-    print(RPC.update(details=f"{entry.get()}", state=f"{entry2.get()}", large_image="example_img", small_image="example_img", large_text="large tooltip", small_text="small tooltip"))
+    try:
+        print(RPC.update(details=f"{entry.get()}", state=f"{entry2.get()}", large_image=f"{large.get()}", small_image=f"{small.get()}", large_text="i cant think of a tooltip :(", small_text="see other tooltip"))  # Set the presence
+    except:
+        try:
+            print(RPC.update(details=f"{entry.get()}", state=f"{entry2.get()}", small_image=f"{small.get()}", large_text="i cant think of a tooltip :(", small_text="see other tooltip"))  # Set the presence
+        except:
+                try:
+                   print(RPC.update(details=f"{entry.get()}", state=f"{entry2.get()}", large_image=f"{large.get()}", large_text="i cant think of a tooltip :(", small_text="see other tooltip"))  # Set the presence 
+                except:
+                    print(RPC.update(details=f"{entry.get()}", state=f"{entry2.get()}", large_text="i cant think of a tooltip :(", small_text="see other tooltip"))  # Set the presence 
+
+def close():
+    sys.exit(1) 
+
 
 entry.grid(row=1, column=0)
 entry2.grid(row=1, column=1)
-
+small.grid(row=3, column=0)
+large.grid(row=3, column=1)
 btn = tk.Button(master=root, text="set rpc", command=conv)
-btn.grid(row=2, column=0)
+btn.grid(row=1, column=2)
+close_button = tk.Button(master=root, text="quit", command=close)
+close_button.grid(row=3, column=2)
 
 root.mainloop()
